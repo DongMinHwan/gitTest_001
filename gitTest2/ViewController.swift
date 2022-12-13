cb55398bf0b92438bd16a3dc1f68856bfb014646
DongMin Hwang <hdmin8712@gmail.com>
2022-12-13 21:20:49 +0900

    Initial Commit

---
+++ gitTest2/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  gitTest2
+//
+//  Created by DongMin Hwang on 2022/12/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func testBtn(_ sender: Any) {
+        
+        print("gitCheck")
+    }
+    
+}
+
